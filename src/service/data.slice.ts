d97c75283a17814b7b24debb6cd1026d458a73c2
Davide Arcinotti <mastrobardo@gmail.com>
2022-07-23 17:01:36 +0200

    feat: add api for games

---
+++ src/service/data.slice.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {IGame} from '@interfaces/game.interface';
+
+const baseUrl: string = 'http://localhost:9000/';
+export const gameApi = createApi({
+  reducerPath: 'gameApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+  }),
+  endpoints: (builder) => ({
+    getGames: builder.query<IGame[], void>({
+      query: () => 'games',
+    }),
+    getGameById: builder.query<IGame, string>({
+      query: (id: string) => `games/?id=${id}`,
+    }),
+  }),
+});
+ 
+export const {
+  useGetGamesQuery,
+  useGetGameByIdQuery,
+} = gameApi;
