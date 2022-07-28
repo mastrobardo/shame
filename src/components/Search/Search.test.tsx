55b9e85cfc01bbb003e9d18ca3de471d2db6b62a
Davide Arcinotti <mastrobardo@gmail.com>
2022-07-28 12:34:54 +0200

    test: update for debounced input

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,17 +1,21 @@
 import { renderWithProviders } from '@utils/test-utils';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import { Search } from './Search';
 
 describe.only('Search', () => {
-  test.only('should only render the visible elements in the grid', async () => {
+  test.only('should change state on type', async () => {
 
-    const { getByRole, store } = renderWithProviders(<Search />, {
+    const { store } = renderWithProviders(<Search />, {
       //@ts-ignore
-      preloadedState: { value:'' }
+      preloadedState: { value:'' },
     });
 
     userEvent.type(screen.getByRole('input'), 'Tiger');
-    expect(await store.getState().search.value).toEqual('Tiger')
+    setTimeout(() => {
+      //this field is debounced
+      expect(store.getState().search.value).toEqual('Tiger');
+    }, 1000)
+    
   });
 });
