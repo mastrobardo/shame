24bc10a7ad58258c03fc4213cfd6477b7738f75b
Davide Arcinotti <mastrobardo@gmail.com>
2022-07-28 18:38:42 +0200

    feat: add raw styling for searchbox

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import { Search } from '@components/Search/Search';
+import { ReactComponent as Logo } from '@assets/svg/logo.svg';
+import './Header.style.scss';
 
 export const Header = () => {
-  return <div className="header">
-        <Search />
-    </div>;
+  return (<div className="header">
+    <Logo />
+    <Search />
+  </div>);
 };
